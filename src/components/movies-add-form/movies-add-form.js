f76a0d3cbe20c981ec6b8bad859f1a6033c85505
Erkinovadham <adhamjonerkinov65@gmail.com>
2023-04-28 21:30:42 +0500

    proget

--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -1,7 +1,18 @@
-const MoviesAddForm = () => {
-  return (
-    <div>MoviesAddForm</div>
-  )
-}
+import "./movies-add-form.css"
 
-export default MoviesAddForm
+const MoviesAddForm = () => {
+  return <div className="movies-add-form">
+    <h3>
+       Yangi kino qo'shish
+    </h3>
+    <form className="add-form d-flex">
+         <input type="text" className="form-control new-post-label" placeholder="Qanday kino?" />
+         <input type="number" className="form-control new-post-label" placeholder="Nechi marotaba ko'rilgan" />
+         <button type="submit" className="btn btn-outline-dark">
+          Qo'shish
+         </button>
+      </form>   
+    </div>;
+};
+
+export default MoviesAddForm;
